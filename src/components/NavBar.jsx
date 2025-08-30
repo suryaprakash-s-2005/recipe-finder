d0e532f46f78d0434972efb7781b2a37ab4190cc
suryaprakash-s-2005 <23cseb57.suryaprakash.s@gmail.com>
2025-08-31 00:38:32 +0530

    css fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import "../styles/Navbar.css";
+import "../styles/NavBar.css";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
